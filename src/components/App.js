202048b4999b937a38df682277fe981d7c5cb687
samuel mckaig <bboysamuel@hotmail.com>
2021-02-05 12:04:37 -0800

    create: api import successful. created init load to import and set users array to state.

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,41 @@
 
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
+const BASE_API = 'https://randomuser.me/api'
 
-const App = (props) => {
+import {
+// components here
+} from './'
+
+
+const App = () => {
+  const [users, setUsers] = useState([])
+
+
+  const handleInitialLoad = async () => {
+    try {
+      const { data } = await axios.get(`${BASE_API}/?results=11`)
+      console.log('data in app.js', data)
+      console.log('data.results in app.js', data.results)
+      setUsers(data.results)
+
+    } catch (error) {
+      throw error
+    }
+  }
+
+  useEffect(() => {
+    handleInitialLoad()
+  }, [])
+
+  // users comes as array of obj. Destructure for use. pass in as props
 
 
   return(<>
-  <div>
+  <div className="app">
   <p>test display in App component</p>
   </div>
 
